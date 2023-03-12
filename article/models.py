1b56187150ceaaecc93bc7b20a1b5499a8d4d073
om3rd <omer-ucuncu@hotmail.com>
2023-03-12 23:18:30 +0300

    [feature]-[9]-[semver:minor]-Database Schema and Articles Page Layout Changed.

--- article/models.py
+++ article/models.py
@@ -8,10 +8,12 @@ class Article(models.Model):  #djangonun kendi tablosu olan auth user ı aldık
     author = models.ForeignKey("auth.User",on_delete = models.CASCADE) 
     title = models.CharField(max_length = 50) 
     content = RichTextField()
+    subtitle = models.CharField(max_length=180, null= True)
     created_date = models.DateField(auto_now_add = True)
     linkedin = models.URLField(null = False, blank = True, max_length=200)
     github = models.URLField(max_length = 200, null = False, blank = True)
     article_image = models.FileField(blank = True, null = True,verbose_name ="Add photo into article")
+    
     
     def __str__(self):  #admin panalindeki articles object yerine direkt title kendi döncek
         return self.title
